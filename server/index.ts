0a089a82e5a82f12ffbcfedc98cb9c27602598a1
Mohammad Zaid Shaikh <zaidshaikhhulk@gmail.com>
2025-01-20 10:44:13 +0530

    Hono Error Handaling

--- server/index.ts
+++ server/index.ts
@@ -1,9 +1,39 @@
+import type { ErrorResponse } from '@/shared/types'
 import { Hono } from 'hono'
+import { HTTPException } from 'hono/http-exception'
 
 const app = new Hono()
 
 app.get('/', (c) => {
+
+
+  throw new HTTPException(404,{message:"Not Found"})
+
+
   return c.text('Hello Hono!')
 })
+
+app.onError((err, c) => {
+ if(err instanceof HTTPException){
+  const ErrorResponse=err.res ?? c.json<ErrorResponse>({
+    success:false,
+    error:err.message,
+    isFormError:err.cause && typeof err.cause === 'object' && "form" in err.cause 
+    ?err.cause.form==true :false
+  },err.status);
+
+  return ErrorResponse;
+ }
+
+  return c.json<ErrorResponse>({
+    success:false,
+    error:process.env.NODE_ENV==="development" ?  (err.stack ?? err.message) : "Internal Server Error",
+    isFormError:false
+  },500);
+
+
+})
+
+
 
 export default app
